086c9f7c17ba18e07e98426a86040e3f1cd3c07f
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-11-02 12:59:58 +0100

    Adding commands on how to evaluate lisp code

--- how-tos.el
+++ how-tos.el
@@ -1,3 +1,9 @@
 ;;; Start emacs without the init.el
 
 emacs -q ; Do not load an init file
+
+
+;;; How to evaluate lisp code
+
+C-x C-e; eval-last-sexp; 
+C-M-x ; eval-defun; it evaluates for the outermost form your point is in
