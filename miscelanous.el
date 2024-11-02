04d34aff9140654f28da37bf8e6088a97e8257ce
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-11-03 00:46:40 +0100

    Adding apropos

--- miscelanous.el
+++ miscelanous.el
@@ -1,3 +1,6 @@
 C-x * * ; Emacs calculator! TODO: Read more about this
 
 C-h i ; Displays the info manual. This list contains all TeXinfo manuals available in your computer
+
+C-h a ; Opens apropos, a search engine that, given a word, narrows all the commands related to that word
+;; Try it out By doing C-h a -word$ (all commands that finish with words)
