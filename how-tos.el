a711dea767e8c22a592ab34b6313d3c9ee5ec0bc
Andrés Laurito Giber <andy.laurito@gmail.com>
2024-11-20 01:10:31 +0100

    Adding name of the command

--- how-tos.el
+++ how-tos.el
@@ -35,4 +35,5 @@ C-x * * ; Opens emacs calculator
 g d ; evil-goto-definition, go to the definition of this class/method
 C-o ; evil-jump-backward; Go back to older position in jump list
 
+;;; (count-words-region)
 C-= ; Counts number of lines, sentences, words in current buffer
