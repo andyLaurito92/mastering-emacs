2b833033aaa39b4c7c6ece82d83e5983970909d4
Andrés Laurito Giber <andy.laurito@gmail.com>
2025-01-20 05:48:22 +0100

    Major mode info

---
+++ major-mode-load-order.el
@@ -0,0 +1,9 @@
+;;; File-local variables
+
+;;; Are variables that emacs can enable per-file if they present in the file. They can appear
+;;; as headers
+;;; Example 
+-*- mode: python ; my-variable: -*-
+
+;;; Variable containing the map of regex-maches -> mode-to-use accordingly
+auto-mode-alist ; alist is associative list
